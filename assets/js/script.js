1ff830aae7271b1b79ce53449dfb0b2b451e2217
Lacey <lacey.griffith@g.austincc.edu>
2021-03-11 11:52:03 -0600

    timer works and question appears

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,40 +8,63 @@
 //when over, score is shown
 //type initials and score is saved
 
+var answerContainerEl = document.querySelector("#quiz-answers");
+var answerContainerHideEl = document.querySelector("#hide");
+
 //create button to start the quiz
 var startButton = document.createElement("button");
 startButton = document.querySelector("#start-button");
 startButton.className = "btn";
 startButton.textContent = "Click to start";
 
-var answerContainerEl = document.getElementById("quiz-answers");
 
 
 var score = 15;
 
-var timeLeft = 50
+var timeLeft = 10
 timer.textContent = "Time Left:" + timeLeft;
 
 //array of questions
-var questionsArray = [
-    {
+var questionsArray = [{
         number: 1,
         question: "Which method would you use before saving an object to localStorage?",
-        answers: [
-            {text: "JSON.stringify(arrayName)", correct: true},
-            {text: "Wrong answer", correct: false},
-            {text: "Wrong Again", correct: false},
-            {text: "And wrong again", correct: false}
+        answers: [{
+                text: "JSON.stringify(arrayName) (this is correct)",
+                correct: true
+            },
+            {
+                text: "Wrong answer",
+                correct: false
+            },
+            {
+                text: "Wrong Again",
+                correct: false
+            },
+            {
+                text: "And wrong again",
+                correct: false
+            }
         ]
     },
     {
         number: 2,
         question: "How would you style an element to have pink font?",
-        answers: [
-            {text: "sdfsfsdf", correct: false},
-            {text: "Wrong answer", correct: false},
-            {text: "Wrong Again", correct: false},
-            {text: "And correct!", correct: true}
+        answers: [{
+                text: "this is an inccorrect answer",
+                correct: false
+            },
+            {
+                text: "Wrong answer",
+                correct: false
+            },
+            {
+                text: "Wrong Again",
+                correct: false
+            },
+            {
+                text: "And correct!",
+                correct: true
+            }
         ]
     },
 ];
@@ -52,8 +75,7 @@ function runTimer() {
     timeLeft--;
     if (timeLeft > 0) {
         setTimeout(runTimer, 1000)
-    }
-    else if (timeLeft === 0) {
+    } else if (timeLeft === 0) {
         alert("You ran out of time!");
         //endquiz();
     }
@@ -64,48 +86,35 @@ function runTimer() {
 //when start button is clicked , create questions
 function startQuiz() {
     startButton.remove();
-    answerContainerEl.classList.remove("hide");
-    createQuestion(questionsArray);
-}
-
-function createQuestion (questionsArray) {
-    //ref div to hold questions and add to div quiz content
-    const quizQuestion = document.querySelector("#quiz-questions");
-    quizQuestion.className = "quiz-questions";
-
-    for(var i = 0; i < questionsArray.length; i++) {
-    const answerButton = document.createElement("button");
-    answerButton.className = "btn";
-    answerButton.textContent = answers.text; //showing as undefined
-    }
-
-
-    for (var i = 0; i < questionsArray.length; i++) {
-        quizQuestion.textContent = questionsArray[i].number +". " + questionsArray[i].question;
-        //if correct answer is chosen, go to next question and award points
-        //if incorrect answer is chosen, deduct time from timer
-        if(true) {
-            score = score + 5;
-        } 
-        else {
-            timeLeft= timeLeft - 10;
-            score = score - 5;
+    console.log(questionsArray.answers);
+    for (var i=0; i < questionsArray.length; i++) {
+    var createQuestion = document.querySelector("#quiz-questions");
+    createQuestion.textContent = questionsArray[i].question
+    
+        for (var i=0; i < questionsArray.answers.length; i++) {
+        var createAnswers = document.createElement("button");
+        createAnswers.className("quiz-answers btn");
+        createAnswers.textContent = "testing" + questionsArray.answers[i];
         }
     }
 }
 
 
-//display score
 
-//prompt user for initals
-
-//save initals and score to localStorage
-
-//on submit, show highscores
+    //if incorrect answer is chosen, deduct time from timer
 
 
-//eventlistener for start button
-startButton.addEventListener("click",runTimer);
-startButton.addEventListener("click", startQuiz);
+    //display score
 
-//event listeners for answer buttons
+    //prompt user for initals
+
+    //save initals and score to localStorage
+
+    //on submit, show highscores
+
+
+    //eventlistener for start button
+    startButton.addEventListener("click", runTimer);
+    startButton.addEventListener("click", startQuiz);
+
+    //event listeners for answer buttons
