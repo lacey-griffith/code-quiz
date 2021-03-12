f0f8ce7033e8afec92425750f4f15df987b5d0de
Lacey <lacey.griffith@g.austincc.edu>
2021-03-12 12:35:16 -0600

    questions display, buttons cooperate, score displays

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,7 +8,7 @@
 //when over, score is shown
 //type initials and score is saved
 
-
+var displayQuestion = document.querySelector("#quiz-questions");
 //create button to start the quiz
 var startButton = document.createElement("button");
 startButton = document.querySelector("#start-button");
@@ -23,45 +23,67 @@ var score = 15;
 var timeLeft = 10
 timer.textContent = "Time Left:" + timeLeft;
 
+var currentQuestion = 0;
 //array of questions
 var questionsArray = [{
         number: 1,
-        question: "Which method would you use before saving an object to localStorage?",
+        question: "Which method would you use before saving an array to localStorage?",
         answers: [{
-                text: "JSON.stringify(arrayName) (this is correct)",
+                text: "JSON.stringify(arrayName)",
                 correct: true
             },
             {
-                text: "Wrong answer",
+                text: "localStorage.setItem(arrayName)",
                 correct: false
             },
             {
-                text: "Wrong Again",
+                text: "JSON.parse(arrayName)",
                 correct: false
             },
             {
-                text: "And wrong again",
+                text: "save.localStorage(arrayName)",
                 correct: false
             }
         ]
     },
     {
         number: 2,
-        question: "How would you style an element to have pink font?",
+        question: "How would you dynamically style an element to have pink font?",
         answers: [{
-                text: "this is an inccorrect answer",
+                text: "setAttribute = 'style', 'pink'",
                 correct: false
             },
             {
-                text: "Wrong answer",
+                text: "elementToStyle {color:pink;}",
                 correct: false
             },
             {
-                text: "Wrong Again",
+                text: "elementToStyle.Color('pink')",
                 correct: false
             },
             {
-                text: "And correct!",
+                text: "elementToStyle.setAttribute('style','color:pink');",
+                correct: true
+            }
+        ]
+    },
+    {
+        number: 3,
+        question: "How do you locate the element that triggers an event?",
+        answers: [{
+                text: "elementEl.addEventListener('click', functionName)",
+                correct: false
+            },
+            {
+                text: "eventListener.target",
+                correct: false
+            },
+            {
+                text: "locate.event",
+                correct: false
+            },
+            {
+                text: "event.target",
                 correct: true
             }
         ]
@@ -82,97 +104,128 @@ function runTimer() {
     console.log(timeLeft);
 }
 
-function startQuiz () {
+function startQuiz() {
     createQuestion();
 }
 
 //when start button is clicked , create questions
 function createQuestion() {
-    startButton.remove();
-    buttonA = document.querySelector("#buttonA").removeAttribute("hide");
-    buttonB = document.querySelector("#buttonB").removeAttribute("hide");
-    buttonC = document.querySelector("#buttonC").removeAttribute("hide");
-    buttonD = document.querySelector("#buttonD").removeAttribute("hide");
+    if (currentQuestion < questionsArray.length) {
+        startButton.remove();
+        buttonA = document.querySelector("#buttonA").removeAttribute("hide");
+        buttonB = document.querySelector("#buttonB").removeAttribute("hide");
+        buttonC = document.querySelector("#buttonC").removeAttribute("hide");
+        buttonD = document.querySelector("#buttonD").removeAttribute("hide");
 
-    for (var i = 0; i < questionsArray.length; i++) {
         answers = [];
-        var createQuestion = document.querySelector("#quiz-questions");
-        createQuestion.textContent = questionsArray[i].number + ". " + questionsArray[i].question
-        answers.push(questionsArray[i].answers);
+        //displayQuestion = document.querySelector("#quiz-questions");
+        displayQuestion.textContent = questionsArray[currentQuestion].number + ". " + questionsArray[currentQuestion].question
+        answers.push(questionsArray[currentQuestion].answers);
+
+        var buttonA = document.querySelector("#buttonA");
+        buttonA.className = "btn";
+        buttonA.textContent = "A) " + answers[0][0].text;
+        buttonA.value = answers[0][0].correct
+
+        var buttonB = document.querySelector("#buttonB");
+        buttonB.className = "btn";
+        buttonB.textContent = "B) " + answers[0][1].text;
+        buttonB.value = answers[0][1].correct
+
+        var buttonC = document.querySelector("#buttonC");
+        buttonC.className = "btn";
+        buttonC.textContent = "C) " + answers[0][2].text;
+        buttonC.value = answers[0][2].correct;
+
+        var buttonD = document.querySelector("#buttonD");
+        buttonD.className = "btn";
+        buttonD.textContent = "D) " + answers[0][3].text;
+        buttonD.value = answers[0][3].correct;
+        currentQuestion++
+    } else if (currentQuestion >= questionsArray.length) {
+        alert("quiz is over! Your score is");
+        displayScore();
     }
 
-    var buttonA = document.querySelector("#buttonA");
-    buttonA.className = "btn";
-    buttonA.textContent = "A) " + answers[0][0].text;
-    buttonA.value = answers[0][0].correct
-
-    var buttonB = document.querySelector("#buttonB");
-    buttonB.className = "btn";
-    buttonB.textContent = "B) " + answers[0][1].text;
-    buttonB.value = answers[0][1].correct
-
-    var buttonC = document.querySelector("#buttonC");
-    buttonC.className = "btn";
-    buttonC.textContent = "C) " + answers[0][2].text;
-    buttonC.value = answers[0][2].correct;
-
-    var buttonD = document.querySelector("#buttonD");
-    buttonD.className = "btn";
-    buttonD.textContent = "D) " + answers[0][3].text;
-    buttonD.value = answers[0][3].correct;
 };
 
+function displayScore() {
+    displayQuestion.remove();
+    buttonA.remove();
+    buttonB.remove();
+    buttonC.remove();
+    buttonD.remove();
+
+    var name = prompt("Enter your intials to save your score!")
+
+    var viewScore = document.querySelector("#view-score")
+    viewScore.textContent = name + "   " + score;
+    localStorage.setItem("name", name);
+    localStorage.setItem("score", score);
+}
 
 
 function checkAnswerA(event) {
     if (
         buttonA.value === "true") {
-        alert("Right!");
+        var feedBack = document.querySelector("#question-feedback")
+        feedBack.textContent = "Correct!";
         score = score + 5;
+        createQuestion();
 
     } else if (
         buttonA.value === "false") {
         alert("Wrong!");
         timeLeft = timeLeft - 2;
+        createQuestion();
     }
 }
 
 function checkAnswerB(event) {
     if (
         buttonB.value === "true") {
-        alert("Right!");
+        var feedBack = document.querySelector("#question-feedback")
+        feedBack.textContent = "Correct!";
         score = score + 5;
+        createQuestion();
 
     } else if (
         buttonB.value === "false") {
         alert("Wrong!");
         timeLeft = timeLeft - 2;
+        createQuestion();
     }
 }
 
 function checkAnswerC(event) {
     if (
         buttonC.value === "true") {
-        alert("Right!");
+        var feedBack = document.querySelector("#question-feedback")
+        feedBack.textContent = "Correct!";
         score = score + 5;
+        createQuestion();
 
     } else if (
         buttonC.value === "false") {
         alert("Wrong!");
         timeLeft = timeLeft - 2;
+        createQuestion();
     }
 }
 
 function checkAnswerD(event) {
     if (
         buttonD.value === "true") {
-        alert("Right!");
+        var feedBack = document.querySelector("#question-feedback")
+        feedBack.textContent = "Correct!";
         score = score + 5;
+        createQuestion();
 
     } else if (
         buttonD.value === "false") {
         alert("Wrong!");
         timeLeft = timeLeft - 2;
+        createQuestion();
     }
 }
 //display score
@@ -191,8 +244,8 @@ startButton.addEventListener("click", startQuiz);
 
 //checkout the console logs for this mess
 buttonA.addEventListener("click", checkAnswerA);
-buttonB.addEventListener("click", checkAnswerB)
-buttonC.addEventListener("click", checkAnswerC)
-buttonD.addEventListener("click", checkAnswerD)
+buttonB.addEventListener("click", checkAnswerB);
+buttonC.addEventListener("click", checkAnswerC);
+buttonD.addEventListener("click", checkAnswerD);
 
 //event listeners for answer buttons
